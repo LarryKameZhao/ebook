5816e56ee5915e163d6d3b2b728a11617a7cfee5
LarryKameZhao <larryzhao1994@outlook.com>
2018-11-28 08:19:47 +0800

    'ebook-init-css'

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './assets/styles/fonts/icon.css'
+import './assets/styles/fonts/reset.scss'
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
